9970ba0ae6e2aa6c592339e5ec2a44edb35b0f3f
25520264-gif <25520264@gm.uit.edu.vn>
2026-03-18 21:49:01 +0700

    Update Sinhvien_diem.cpp

--- 25520264_BTTH1/Sinhvien_diem.cpp
+++ 25520264_BTTH1/Sinhvien_diem.cpp
@@ -15,23 +15,23 @@ struct Sinh_vien
 int main()
 {
     Sinh_vien sv;
-    cout<< "Nhap ho ten: ";
+    cout<< "Nhap ho ten: ";    // Nhap ho ten: Duong Tuan Dat
     cin.getline(sv.s, mx);
 
-    cout<< "Nhap diem toan: ";cin>> sv.t;
-    cout<< "Nhap diem van: ";cin>> sv.v;
+    cout<< "Nhap diem toan: ";cin>> sv.t;    // Nhap diem toan: 10
+    cout<< "Nhap diem van: ";cin>> sv.v;     // Nhap diem van: 9.8
 
-    double dtb = (sv.t+sv.v)/2;
+    double dtb = (sv.t+sv.v)/2;    // dtb = (10 + 9.8)/2 = 9.9 
 
-    cout<< "Ho ten sinh vien: ";
+    cout<< "Ho ten sinh vien: ";    
     int i =0;
-    while(sv.s[i] != '\0')
+    while(sv.s[i] != '\0')    // in Duong Tuan Dat
     {
         cout<< sv.s[i];
         i++;
     }
     cout<< endl;
 
-    cout<< "Diem trung binh: "<< fixed<< setprecision(2)<< dtb;
+    cout<< "Diem trung binh: "<< fixed<< setprecision(2)<< dtb; // vDiem trung binh: 9.90 
 
 }
