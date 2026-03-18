49429d0406bf4106516aa49d575a60fdad619f5a
25520264-gif <25520264@gm.uit.edu.vn>
2026-03-18 21:19:05 +0700

    Update Sosanh_phanso.cpp

--- 25520264_BTTH1/Sosanh_phanso.cpp
+++ 25520264_BTTH1/Sosanh_phanso.cpp
@@ -4,25 +4,28 @@ using namespace std;
 int main()
 {
     int a, b, m ,n;
-    cout<< "Nhap tu 1: ";cin>> a;    
+    cout<< "Nhap tu 1: ";cin>> a;    // Nhap 6
     cout<< "Nhap mau 1: ";
-    while( cin>> b)
+
+    // Kiem mau khac 0
+    while( cin>> b)    // Nhap 9
     {
         if(b != 0) break;
         cout<< "Nhap mau 1: ";
     }
 
-    cout<< "Nhap tu 2: ";cin>> n;
+    cout<< "Nhap tu 2: ";cin>> n;    // Nhap 5
     cout<< "Nhap mau 2: ";
-    while( cin>> m)
+    while( cin>> m)    //Nhap 3
     {
         if(m != 0) break;
         cout<< "Nhap mau 2: ";
     }
 
-    if((a*m) > (n*b))
+    // (a/b) > (n/m) <=> (a*m) > (n*b)
+    if((a*m) > (n*b)) 
         cout<< "Ket qua: "<< a<< "/"<< b;
-    else
-        cout<< "Ket qua: "<< n<< "/"<< m;
-
+    else    // (6*3) < (5*9)
+        cout<< "Ket qua: "<< n<< "/"<< m;     // Ket qua: 5/3
+    return 0;
 }
