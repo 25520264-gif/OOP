6a7783840c7b29974d54102c87c19ebe95b206d8
25520264-gif <25520264@gm.uit.edu.vn>
2026-03-18 21:26:39 +0700

    Update phanso_tonghieutichhthuong.cpp

--- 25520264_BTTH1/phanso_tonghieutichhthuong.cpp
+++ 25520264_BTTH1/phanso_tonghieutichhthuong.cpp
@@ -4,17 +4,19 @@ using namespace std;
 int main()
 {
     int a, b, m ,n;
-    cout<< "Nhap tu 1: ";cin>> a;
+    cout<< "Nhap tu 1: ";cin>> a;    // Nhap 9
     cout<< "Nhap mau 1: ";
-    while( cin>> b)
+
+    // Kiem mau khac 0
+    while( cin>> b)    // Nhap 6
     {
         if(b != 0) break;
         cout<< "Nhap mau 1: ";
     }
 
-    cout<< "Nhap tu 2: ";cin>> n;
+    cout<< "Nhap tu 2: ";cin>> n;    // Nhap 3
     cout<< "Nhap mau 2: ";
-    while( cin>> m)
+    while( cin>> m)    // Nhap 5
     {
         if(m != 0) break;
         cout<< "Nhap mau 2: ";
@@ -22,9 +24,9 @@ int main()
 
     int t;
     cout<< " 1. Tong  2.Hieu  3.Tich  4.Thuong"<< endl;
-    cout<< "Chon: ";cin>> t;
+    cout<< "Chon: ";cin>> t;    // Chon 2
 
-    if(b == n)
+    if(b == n)    // Neu mau bang nhau --> tu so cong tru nhau
     {
         switch(t)
         {
@@ -35,12 +37,12 @@ int main()
         }
     }
 
-    else
+    else    // Mau so khong bang nhau
     {
         switch(t)
         {
             case 1: cout<< "Ket qua: "<< a*m + n*b<< "/"<< b*m;break;
-            case 2: cout<< "Ket qua: "<< a*m - n*b<< "/"<< b*m;break;
+            case 2: cout<< "Ket qua: "<< a*m - n*b<< "/"<< b*m;break;    // Ket qua: 27/30
             case 3: cout<< "Ket qua: "<< a*n<< "/"<< b*m;break;
             case 4: cout<< "Ket qua: "<< a*m<< "/"<< b*n;break;
         }
